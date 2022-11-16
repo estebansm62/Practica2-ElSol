804f09f693cfc76f628b286b607548854f367648
Esteban <estebansm62@gmail.com>
2022-11-16 12:53:06 +0100

    vcs

--- app/src/main/java/com/example/practica2_elsol_ignat/CompararActivity.kt
+++ app/src/main/java/com/example/practica2_elsol_ignat/CompararActivity.kt
@@ -2,47 +2,42 @@ package com.example.practica2_elsol_ignat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.TextView
 
 class CompararActivity : AppCompatActivity() {
-    private lateinit var nameTextField2: AutoCompleteTextView
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_comparar)
 
-        val nameTextField1 = findViewById<AutoCompleteTextView>(R.id.planeta1)
+        val planeta1 = findViewById<AutoCompleteTextView>(R.id.planeta1)
+        planeta1.threshold = 1
 
         val diametro1 = findViewById<TextView>(R.id.diamPlaneta1)
         val distancia1 = findViewById<TextView>(R.id.distPlaneta1)
         val densidad1 =  findViewById<TextView>(R.id.densPlanetas1)
 
-        val nameTextField2 = findViewById<AutoCompleteTextView>(R.id.planeta2)
+        val planeta2 = findViewById<AutoCompleteTextView>(R.id.planeta2)
+        planeta2.threshold = 1
 
         val diametro2 = findViewById<TextView>(R.id.diamPlaneta2)
         val distancia2 = findViewById<TextView>(R.id.distPlaneta2)
         val densidad2 =  findViewById<TextView>(R.id.densPlanetas2)
 
 
-
-
         PlanetaAdapter(this, Planeta.planetas).also { adapter ->
-            nameTextField1.setAdapter(adapter)
-            nameTextField2.setAdapter(adapter)
+            planeta1.setAdapter(adapter)
+            planeta2.setAdapter(adapter)
         }
 
-        nameTextField1.setOnItemClickListener { parent, _, position, _ ->
+        planeta1.setOnItemClickListener { parent, _, position, _ ->
             val product = parent.adapter.getItem(position) as Planeta
             diametro1.text = product.diam.toString()
             distancia1.text = product.dist.toString()
             densidad1.text = product.dens.toString()
         }
 
-        nameTextField2.setOnItemClickListener { parent, _, position, _ ->
+        planeta2.setOnItemClickListener { parent, _, position, _ ->
             val product = parent.adapter.getItem(position) as Planeta
             diametro2.text = product.diam.toString()
             distancia2.text = product.dist.toString()
--- app/src/main/java/com/example/practica2_elsol_ignat/PlanetaAdapter.kt
+++ app/src/main/java/com/example/practica2_elsol_ignat/PlanetaAdapter.kt
@@ -7,7 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class PlanetaAdapter(context: Context, items: List<Planeta>):ArrayAdapter<Planeta>(context, 0, items){
+class PlanetaAdapter(context: Context, items: List<Planeta>): ArrayAdapter<Planeta>(context, 0, items){
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val view = convertView ?: LayoutInflater.from(context)
